8abd39581fdc4f8c9262c9f13f14a09048ecc496
nanokozmos <ovuncuzun@gmail.com>
2023-12-06 16:36:17 +0000

    improving the day 6 solution

--- day6/day6.js
+++ day6/day6.js
@@ -4,52 +4,53 @@ https://adventofcode.com/2023/day/6
 
 const fs = require("fs");
 
-function extractValues(line) {
-    return (line.match(/\d+/g) || []).map(Number);
-}
+const extractValues = (line) => (line.match(/\d+/g) || []).map(Number);
 
-function calculateWaysToWin(times, distances) {
-    let total = 1;
-
-    for (let i = 0; i < times.length; i++) {
-        let waysToWin = 0;
-        for (let buttonLength = 0; buttonLength < times[i]; buttonLength++) {
-            if ((times[i] - buttonLength) * buttonLength > distances[i]) {
-                waysToWin++;
-            }
-        }
-        total *= waysToWin;
-    }
-
-    return total;
-}
-
-function partOne(lines) {
-    const times = extractValues(lines[0]);
-    const distances = extractValues(lines[1]);
-
-    return calculateWaysToWin(times, distances);
-}
-
-function partTwo(lines) {
-    const times = extractValues(lines[0]);
-    const distances = extractValues(lines[1]);
-
-    const time = parseInt(times.join(''), 10);
-    const distance = parseInt(distances.join(''), 10);
-
+const getWaysToWin = (time, distance) => {
     let waysToWin = 0;
     for (let buttonLength = 0; buttonLength < time; buttonLength++) {
         if ((time - buttonLength) * buttonLength > distance) {
             waysToWin++;
         }
     }
-
     return waysToWin;
-}
+};
+
+const calculatePart = (lines, partNumber) => {
+    const times = extractValues(lines[0]);
+    const distances = extractValues(lines[1]);
+
+    let values;
+    if (partNumber === 1) {
+        values = { times, distances };
+    } else {
+        values = {
+            time: parseInt(times.join(''), 10),
+            distance: parseInt(distances.join(''), 10),
+        };
+    }
+
+    let total = 1;
+
+    if (partNumber === 1) {
+        for (let i = 0; i < values.times.length; i++) {
+            let waysToWin = getWaysToWin(values.times[i], values.distances[i]);
+            total *= waysToWin;
+        }
+    } else {
+        total = getWaysToWin(values.time, values.distance);
+    }
+
+    return total;
+};
 
 fs.readFile("input.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.error("Error reading the file:", err);
+        return;
+    }
+
     const lines = data.split("\n");
-    console.log(`Part One: ${partOne(lines)}`); // 5133600
-    console.log(`Part One: ${partTwo(lines)}`); // 40651271
+    console.log(`Part One: ${calculatePart(lines, 1)}`); // 5133600
+    console.log(`Part Two: ${calculatePart(lines, 2)}`); // 40651271
 });
